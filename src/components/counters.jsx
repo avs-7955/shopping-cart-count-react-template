33b73f25e3b9201ead19eff8a3767a853d2d9b5b
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2022-10-29 00:20:27 +0530

    reset btn + refactoring

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -13,12 +13,31 @@ export class Counters extends Component {
     render() {
         return (
             <div>
-                {this.state.counters.map(counter => <Counter key={counter.id} counter={counter} onDelete={this.handleDelete} />)}
+                <button onClick={this.handleReset} className='btn btn-primary btn-sm m-2' >Reset</button>
+                <br />
+                {this.state.counters.map(counter => <Counter key={counter.id} counter={counter} onIncrement={this.handleIncrement} onDelete={this.handleDelete} />)}
             </div>
         )
     }
+
+    handleIncrement = (counter) => {
+        const counters = [...this.state.counters]
+        const index = counters.indexOf(counter)
+        counters[index] = { ...counter }
+        counters[index].value++
+        this.setState({ counters })
+    }
+
     handleDelete = (counterId) => {
         const counters = this.state.counters.filter(c => c.id !== counterId)
+        this.setState({ counters })
+    }
+
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0
+            return c
+        })
         this.setState({ counters })
     }
 }
